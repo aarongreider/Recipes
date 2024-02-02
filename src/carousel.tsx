cdcdba7f82d33172e56cf2b8dd11d18d209f2824
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2024-02-02 13:41:26 -0500

    button styling

--- src/carousel.tsx
+++ src/carousel.tsx
@@ -7,6 +7,17 @@ interface CarouselProps {
 
 function Carousel({ images }: CarouselProps) {
     const [currentIndex, setCurrentIndex] = useState<number>(0);
+    let buttonStyle = {
+        verticalAlign: 'top',
+        font: '900 16px / 1.20 Alegreya Sans, Arial, Helvetica, sans-serif',
+        color: '#30553a',
+        borderRadius: '10px',
+        border: 'none',
+        boxShadow: ' 4px 4px 2px 0 rgba(77, 77, 77, .22)',
+        textDecoration: 'none',
+        background: 'linear-gradient(to bottom, #f3de54 0%, #e2b733 100%)',
+        padding: '10px 15px',
+    }
 
     const nextImage = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -17,12 +28,12 @@ function Carousel({ images }: CarouselProps) {
     };
 
     return (
-        <div style={{ display:'flex',alignItems:'center', justifyContent:'space-around', height:'50vh', maxHeight: '350px', width:'100%', position:'relative'}}>
-            <button onClick={prevImage} style={{}}>{`<`}</button>
-            
-            <img src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} style={{ height:'100%', objectFit: 'contain' }}/>
-            <button onClick={nextImage} style={{}}>{`>`}</button>
-            
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', height: '50vh', maxHeight: '350px', width: '100%', position: 'relative' }}>
+            <button onClick={prevImage} style={buttonStyle}>{`<`}</button>
+
+            <img src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} style={{ height: '100%', objectFit: 'contain', borderRadius:'12px' }} />
+            <button onClick={nextImage} style={buttonStyle}>{`>`}</button>
+
         </div>
     )
 }
